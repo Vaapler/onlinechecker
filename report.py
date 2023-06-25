2baeffbd31a3f0595deccb5ec7c606f856f683a4
Vaapler <46196080+Vaapler@users.noreply.github.com>
2023-06-25 20:49:42 +0300

    Update report.py

--- report.py
+++ report.py
@@ -5,7 +5,8 @@ import json
 
 SECONDS_IN_DAY = 60 * 60 * 24
 DATE_FILE_FORMAT = "%m-%d.txt"
-DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+DATE_INPUT_FORMAT = "%Y-%m-%d %H:%M:%S"
+DATE_OUTPUT_FORMAT = "%H:%M:%S"
 REPORT_PREFIX = "reports/raw/"
 
 report_date = input("Выберите дату в формате месяц-день >> ") + ".txt"
@@ -19,13 +20,13 @@ for service_name in os.listdir(REPORT_PREFIX):
     filename = service_folder + report_date.strftime(DATE_FILE_FORMAT)
     with open(filename, "r") as file:
         down_time = json.load(file)
-    data = [service_name, 0, "", 0]
+    data = [service_name, 0, [], 0]
     for down_interval in down_time:
-        start_time, end_time = [datetime.datetime.strptime(i, DATE_FORMAT) for i in down_interval]
+        start_time, end_time = [datetime.datetime.strptime(i, DATE_INPUT_FORMAT) for i in down_interval]
         data[1] += (end_time - start_time).seconds
-        data[2] += start_time.strftime(DATE_FORMAT) + "-" + end_time.strftime(DATE_FORMAT)
+        data[2] += [start_time.strftime(DATE_OUTPUT_FORMAT), end_time.strftime(DATE_OUTPUT_FORMAT)]
     data[3] = round((SECONDS_IN_DAY - data[1]) / SECONDS_IN_DAY * 100, 2)
-    data[2] = "---" if not data[2] else data[2]
+    data[2] = "---" if not data[2] else "-".join(data[2])
 
     report_table[service_name] = data
 
